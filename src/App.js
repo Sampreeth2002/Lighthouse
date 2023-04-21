365133a4a653a324c36efd93f9d2184858aa3c2a
Sampreeth2002 <sampreeth.2002@gmail.com>
2023-04-21 20:34:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ethers } from "ethers";
+import lighthouse from "@lighthouse-web3/sdk";
 
 function App() {
+  const [fileURL, setFileURL] = React.useState(null);
+
+  const encryptionSignature = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    const messageRequested = (await lighthouse.getAuthMessage(address)).data
+      .message;
+    const signedMessage = await signer.signMessage(messageRequested);
+    return {
+      signedMessage: signedMessage,
+      publicKey: address,
+    };
+  };
+
+  /* Decrypt file */
+  const decrypt = async () => {
+    // Fetch file encryption key
+    const cid = "QmRroPq1oAeKEtG6uTBpiGvU13G3iS3fZxA5Cn7R1HKbPE"; //replace with your IPFS CID
+    const { publicKey, signedMessage } = await encryptionSignature();
+    /*
+      fetchEncryptionKey(cid, publicKey, signedMessage)
+        Parameters:
+          CID: CID of the file to decrypt
+          publicKey: public key of the user who has access to file or owner
+          signedMessage: message signed by the owner of publicKey
+    */
+    const keyObject = await lighthouse.fetchEncryptionKey(
+      cid,
+      publicKey,
+      signedMessage
+    );
+
+    // Decrypt file
+    /*
+      decryptFile(cid, key, mimeType)
+        Parameters:
+          CID: CID of the file to decrypt
+          key: the key to decrypt the file
+          mimeType: default null, mime type of file
+    */
+
+    const fileType = "image/jpeg";
+    const decrypted = await lighthouse.decryptFile(
+      cid,
+      keyObject.data.key,
+      fileType
+    );
+    console.log(decrypted);
+    /*
+      Response: blob
+    */
+
+    // View File
+    const url = URL.createObjectURL(decrypted);
+    console.log(url);
+    setFileURL(url);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <button onClick={() => decrypt()}>decrypt</button>
+      {fileURL ? (
+        <a href={fileURL} target="_blank">
+          viewFile
         </a>
-      </header>
+      ) : null}
     </div>
   );
 }
