a99c6bfbdd7adeaf437aa04317eb15935c34e90d
Sampreeth2002 <sampreeth.2002@gmail.com>
2023-04-22 09:39:17 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { ethers } from "ethers";
 import lighthouse from "@lighthouse-web3/sdk";
 
 function App() {
-  const [fileURL, setFileURL] = React.useState(null);
-
   const encryptionSignature = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
@@ -18,58 +16,20 @@ function App() {
     };
   };
 
-  /* Decrypt file */
-  const decrypt = async () => {
-    // Fetch file encryption key
-    const cid = "QmRroPq1oAeKEtG6uTBpiGvU13G3iS3fZxA5Cn7R1HKbPE"; //replace with your IPFS CID
-    const { publicKey, signedMessage } = await encryptionSignature();
-    /*
-      fetchEncryptionKey(cid, publicKey, signedMessage)
-        Parameters:
-          CID: CID of the file to decrypt
-          publicKey: public key of the user who has access to file or owner
-          signedMessage: message signed by the owner of publicKey
-    */
-    const keyObject = await lighthouse.fetchEncryptionKey(
-      cid,
-      publicKey,
-      signedMessage
+  const uploadText = async () => {
+    const sig = await encryptionSignature();
+    const response = await lighthouse.textUploadEncrypted(
+      "This is a testing string",
+      "7f431ff1.e8fc8d458b6645348cb20014441117a8",
+      sig.publicKey,
+      sig.signedMessage
     );
-
-    // Decrypt file
-    /*
-      decryptFile(cid, key, mimeType)
-        Parameters:
-          CID: CID of the file to decrypt
-          key: the key to decrypt the file
-          mimeType: default null, mime type of file
-    */
-
-    const fileType = "image/jpeg";
-    const decrypted = await lighthouse.decryptFile(
-      cid,
-      keyObject.data.key,
-      fileType
-    );
-    console.log(decrypted);
-    /*
-      Response: blob
-    */
-
-    // View File
-    const url = URL.createObjectURL(decrypted);
-    console.log(url);
-    setFileURL(url);
+    console.log(response);
   };
 
   return (
     <div className="App">
-      <button onClick={() => decrypt()}>decrypt</button>
-      {fileURL ? (
-        <a href={fileURL} target="_blank">
-          viewFile
-        </a>
-      ) : null}
+      <button onClick={() => uploadText()}> Click here</button>
     </div>
   );
 }
